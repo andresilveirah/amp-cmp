cd46a41e6bcb7758c44c0dda794b315e900406d0
André Herculano <andresilveirah@gmail.com>
2019-02-20 17:44:05 +0100

    add CORS header

--- index.js
+++ index.js
@@ -23,12 +23,18 @@ app.post('/consents/check', function(req, res) {
   // Necessary for AMP CORS security protocol.
   // @see https://github.com/ampproject/amphtml/blob/master/spec/amp-cors-requests.md
   res.setHeader('AMP-Access-Control-Allow-Source-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+  res.setHeader('Access-Control-Allow-Origin', req.query.allowedDomain);
 
-  if (consents[req.body.consentInstanceId]) {
-    res.status(200).json({ "promptIfUnknown": consents[req.body.consentInstanceId] });
+  if (req.body.consentInstanceId && consents[req.body.consentInstanceId]) {
+    res.status(200).json({ "promptIfUnknown": consents[req.body.consentInstanceId].consented });
   } else {
     res.status(400).json({ error: 'Key '+req.body.consentInstanceId+' Not Found' });
   }
+});
+
+app.get('/version', function (_req, res) {
+  res.status(200).json("1");
 });
 
 app.listen(config.port, function () {
