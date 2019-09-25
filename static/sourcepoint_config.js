e813efca3282409226122b6c169fd7d13b0d3b37
André Herculano <andresilveirah@gmail.com>
2019-09-25 19:46:46 +0200

    refactor sourcepoint_config

--- static/sourcepoint_config.js
+++ static/sourcepoint_config.js
@@ -3,14 +3,16 @@
   funcName = funcName || "_sp_";
   baseObj = baseObj || window;
 
-  console.log("== Loading AMP Web Page v1 ==");
-  console.log("config from AMP: " + window.name);
+  var siteHref = function(siteName) {
+    return "https://"+siteName;
+  }
+
   var clientConfig = window.amp.config.clientConfig;
 
   baseObj[funcName] = {
     config: {
       accountId: clientConfig.accountId,
-      siteHref: "https://"+clientConfig.siteName,
+      siteHref: siteHref(clientConfig.siteName),
       mmsDomain: window._SP_CONSTANTS.MMS_DOMAIN,
       cmpOrigin: window._SP_CONSTANTS.CMP_ORIGIN,
       waitForConsent: true,
