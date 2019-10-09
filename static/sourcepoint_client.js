f2a1e5365943f160102ead14617015f8d822810b
André Herculano <andresilveirah@gmail.com>
2019-10-09 18:04:22 +0200

    fix plug & play PM
    - add `siteId` and `privacyManagerId` to ` _sp.config`
    - add `_sp_runMessaging` and `_sp_showPM` query params to trigger
    the message or the PM directly
    - remove call to `loadPrivacyManagerModal` on `onSPPMObjectReady`

--- static/sourcepoint_client.js
+++ static/sourcepoint_client.js
@@ -7,46 +7,37 @@
     var purposeConsent = "none";
     return {
       onMessageReady: function () {
-        // only called if the message is going to be shown
         console.log("onMessageReady: "+JSON.stringify(arguments));
-
         amp.show();
       },
-      onMessageChoiceSelect: function (actionId) {
-        // called when an action is taken within the Message
-        console.log("onMessageChoiceSelect: "+JSON.stringify(arguments));
-
-        // TODO: check if the close message button will always have the same actionId
-        if(actionId === 235561) amp.dismiss();
-      },
-      onMessageChoiceError: function (error) {
-        console.log("onMessageChoiceError: "+JSON.stringify(arguments));
+      onMessageChoiceError: function (_error) {
+        console.error("onMessageChoiceError: arguments: "+JSON.stringify(arguments));
         amp.dismiss();
       },
       onSPPMObjectReady: function() {
-        console.log("onSPPMObjectReady: "+JSON.stringify(arguments))
+        console.log("onSPPMObjectReady: arguments: "+JSON.stringify(arguments))
         // called when loadPrivacyManagerModal is available in the _sp_ object
         if(amp.userTriggered()) {
           amp.show();
-          window.onDocumentReady(function() {
-            window._sp_.loadPrivacyManagerModal(siteId, pmId);
-          });
         }
       },
-      onPrivacyManagerAction: function (consents) { // {"purposeConsent":"all|some|none", "vendorConsent":"all|some|none" }
+      onPrivacyManagerAction: function (consents) {
+        // consents: {"purposeConsent":"all|some|none", "vendorConsent":"all|some|none" }
         // called when the user has taken any action within the PM
-        console.log("onPrivacyManagerAction: "+JSON.stringify(arguments));
+        console.log("[onPrivacyManagerAction] arguments: "+JSON.stringify(arguments));
         purposeConsent = consents.purposeConsent
       },
       onPMCancel: function () {
         // called when the user clicks on Cancel within the PM
-        console.log("onPMCancel: "+JSON.stringify(arguments));
+        console.log("[onPMCancel] arguments: "+JSON.stringify(arguments));
         if(amp.userTriggered()) amp.dismiss();
       },
       onConsentReady: function (consentUUID, euconsent) {
         // called once when the user is done interacting with the message OR the PM
-        console.log("onConsentReady: "+JSON.stringify(arguments));
-        purposeConsent === "all" ? amp.accept(euconsent) : amp.reject(euconsent);
+        console.log("[onConsentReady] consentUUID: "+consentUUID+" euconsent: "+euconsent);
+        purposeConsent === "all" ?
+          amp.accept(euconsent) :
+          amp.reject(euconsent);
       },
     }
   }
--- static/sourcepoint_config.js
+++ static/sourcepoint_config.js
@@ -5,13 +5,23 @@
 
   var siteHref = function(siteName) {
     return "https://"+siteName;
-  }
+  };
 
   var clientConfig = window.amp.config.clientConfig;
+
+  // set query params for triggering the message or the PM directly
+  if (history && history.pushState) {
+    var newurl = location.protocol + "//" + location.host + location.pathname
+      + '?_sp_showPM='+window.amp.userTriggered()
+      + '&_sp_runMessaging='+!window.amp.userTriggered();
+    history.pushState({ path: newurl }, '', newurl);
+  }
 
   baseObj[funcName] = {
     config: {
       accountId: clientConfig.accountId,
+      siteId: clientConfig.siteId,
+      privacyManagerId: clientConfig.privacyManagerId,
       siteHref: siteHref(clientConfig.siteName),
       mmsDomain: window._SP_CONSTANTS.MMS_DOMAIN,
       cmpOrigin: window._SP_CONSTANTS.CMP_ORIGIN,
@@ -19,5 +29,5 @@
       targetingParams: clientConfig.targetingParams || {},
       events: window.SourcePointClient(amp, clientConfig.siteId, clientConfig.privacyManagerId)
     }
-  }
+  };
 })("_sp_", window);
