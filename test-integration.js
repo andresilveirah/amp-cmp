4665b998bbc3f649be2db1e050e5a3e8f4d039c0
Daniel Taraschi <daniel@sourcepoint.com>
2021-11-23 15:10:17 -0500

    SP-8103 use script to run tests

--- test-integration.js
+++ test-integration.js
@@ -1,6 +1,25 @@
+const npm = require('npm');
 const { spawn } = require('child_process');
 
 const server = spawn('npm', ['run', 'server'])
 const ui = spawn('npm', ['run', 'ui'])
 
-const jest = spawn('jest', ['--testPathPattern', './tests/integration', '--verbose', '--colors'])
+// const jest = spawn('jest', ['--testPathPattern', './tests/integration', '--verbose', '--colors'])
+
+npm.load(
+    () => npm.run(
+        "jest-integration", 
+        (result) => { 
+            server.kill()
+            ui.kill()
+
+            if(result && result.errno) {
+                process.exit(1);
+            }
+            process.exit();
+        }
+    ) 
+)
+
+// server.kill()
+// ui.kill()
